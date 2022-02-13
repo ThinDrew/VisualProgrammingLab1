8403450b86071e8223a62f40fe78d739c29d69ee
Андрей Никоноров <andreynikonorov5@gmail.com>
2022-02-13 22:01:19 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Task1;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HW1 test = new HW1();
+            int[] a = { 5, 3, 4 };
+            Console.WriteLine(test.QueueTime(a, 1));
+        }
+    }
+}
---
+++ task1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task1
+{
+    class HW1
+    {
+        public long QueueTime(int[] customers, int n)
+        {
+            if (n < 1) throw new Exception("Введите корректное количество касс");
+
+            int[] cashbox = new int[n];
+            int maxTime = 0;
+
+            if (n == 1)
+            {
+                for (int i = 0; i < customers.Length; i++)
+                    maxTime += customers[i];
+                return maxTime;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                cashbox[i] = 0;
+            }
+
+            for (int i = 0; i < customers.Length; i++)
+            {
+                int[] min = new int[2] { cashbox[0], 0 };
+                for (int j = 1; j < cashbox.Length; j++)
+                {
+                    if (cashbox[j] < min[0])
+                    {
+                        min[0] = cashbox[j];
+                        min[1] = j;
+                    }
+
+                }
+                cashbox[min[1]] += customers[i];
+            }
+
+            maxTime = cashbox[0];
+
+            for (int i = 1; i < cashbox.Length; i++)
+            {
+                if (cashbox[i] > maxTime) maxTime = cashbox[i];
+            }
+
+            return maxTime;
+        }
+    }
+}
