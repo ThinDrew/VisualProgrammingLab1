a52568a39e07307e9984363f3c00d6750781aea2
Андрей Никоноров <andreynikonorov5@gmail.com>
2022-02-16 22:00:22 +0700

    Add more examples

--- Program.cs
+++ Program.cs
@@ -8,8 +8,14 @@ namespace lab1
         static void Main(string[] args)
         {
             HW1 test = new HW1();
-            int[] a = { 5, 3, 4 };
-            Console.WriteLine(test.QueueTime(a, 1));
+
+            int[] arr1 = { 5, 3, 4 };
+            int[] arr2 = { 10, 5, 6, 7 };
+            int[] arr3 = { 2, 3, 10 };
+
+            Console.WriteLine(test.QueueTime(arr1, 1));
+            Console.WriteLine(test.QueueTime(arr2, 2));
+            Console.WriteLine(test.QueueTime(arr3, 2));
         }
     }
 }
